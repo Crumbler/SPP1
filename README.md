9c37a4fd6d049d6b49ac6cfacbb856d22c292a28
Crumbler <25861574+Crumbler@users.noreply.github.com>
2022-03-17 12:32:40 +0300

    Basic layout

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# SPP1" 
+SPP1
+
+A simple task list with filtering by status and the ability to set the finish date or attach a file.
--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.set('view engine', 'ejs');
 
+app.use('/css', express.static('css'));
+
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+
+  let tagLine = req.query.tagLine;
+
+  if (!req.query.tagLine)
+  {
+      tagLine = 'no tag supplied';
+  }
+
+  res.render('index.ejs', { 
+      tagLine: tagLine
+  } );
 });
 
+app.get('/test/', (req, res) => {
+    let rawdata = fs.readFileSync('tasks.json');
+    let student = JSON.parse(rawdata);
+
+    res.json(student);
+
+    /*if (req.query.something)
+    {
+        res.send(req.query.something);
+    }
+    else
+    {
+        res.send('nothing sent');
+    }*/
+  });
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
