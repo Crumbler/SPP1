aa35b52acdc1fc90b8873967d1452092398d4d94
Crumbler <25861574+Crumbler@users.noreply.github.com>
2022-03-17 15:09:53 +0300

    Set task date

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs');
+const { url } = require('inspector');
 const app = express();
 const port = 80;
 
-app.set('view engine', 'ejs');
+const urlencodedParser = bodyParser.urlencoded({
+  extended: false,
+});
 
+app.set('view engine', 'ejs');
 app.use('/css', express.static('css'));
+
 
 app.get('/', (req, res) => {
   const rawTasks = fs.readFileSync('tasks.json');
   let tasks = JSON.parse(rawTasks);
+  const totalTasks = tasks.length;
 
   if (req.query.filter && req.query.filter !== 'None')
   {
@@ -18,9 +25,40 @@ app.get('/', (req, res) => {
 
   res.render('index.ejs', { 
       tasks: tasks,
-      filter: req.query.filter ?? "None"
+      totalTasks: totalTasks
   } );
 });
+
+
+app.post('/', urlencodedParser, (req, res) => {
+  if (!req.body || !req.body.task) 
+  {
+    return response.sendStatus(400);
+  }
+
+  const rawTasks = fs.readFileSync('tasks.json');
+  let tasks = JSON.parse(rawTasks);
+
+  const taskId = req.body.task;
+
+  if (req.body.date)
+  {
+    tasks[taskId - 1].completionDate = req.body.date;
+  }
+  else
+  {
+    tasks[taskId - 1].completionDate = null;
+  }
+  
+  const writeData = JSON.stringify(tasks, null, 2);
+  fs.writeFileSync('tasks.json', writeData);
+
+  res.render('index.ejs', { 
+      tasks: tasks,
+      totalTasks: tasks.length
+  } );
+})
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
